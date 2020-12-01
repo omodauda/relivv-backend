e6cf1fef87ba41bc55a40a1b5eb4f8bb195b8827
omodauda <omodauda.dl@gmail.com>
2020-12-01 20:11:34 +0100

    create auth model

---
+++ src/models/auth.js
@@ -0,0 +1,51 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema;
+import {profileSchema} from './profile';
+
+const authSchema = new schema({
+
+    method: {
+        type: String,
+        required: true,
+        enum: ["local", "google"]
+    },
+    local:{
+        username: {
+            type: String,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            trim: true,
+            lowercase: true
+        },
+        password: {
+            type: String,
+            minlength: 5
+        }
+    },
+    google: {
+        id: {
+            type: String
+        },
+        email: {
+            type: String,
+            unique: true,
+            lowercase: true
+        }
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "volunteer", "admin"]
+    },
+    profile: profileSchema
+    
+}, {timestamps: true});
+
+const Auth = mongoose.model('Auth', authSchema);
+
+export default Auth;
+
