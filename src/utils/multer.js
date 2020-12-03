f674f8f50e27eea6bedf2c6b5e4f5aeaf9eebdfc
omodauda <omodauda.dl@gmail.com>
2020-12-03 15:29:03 +0100

    create multer upload image

--- src/utils/multer.js
+++ src/utils/multer.js
@@ -2,7 +2,7 @@ import multer from 'multer';
 import path from 'path';
 
 //multer config
-export default multer({
+const multerFileUpload = multer({
     storage: multer.diskStorage({}),
     fileFilter: (req, file, cb) => {
         let _ext = path.extname(file.originalname);
@@ -15,3 +15,22 @@ export default multer({
     },
     limits: {fileSize: 5 * 1024 * 1024}
 });
+
+const multerImageUpload = multer({
+    storage: multer.diskStorage({}),
+    fileFilter: (req, file, cb) => {
+        let _ext = path.extname(file.originalname);
+        const ext = [".jpg", ".jpeg", ".png"];
+        if(!ext.includes(_ext)){
+            cb(new Error("File format not supported"), false);
+            return;
+        }
+        cb(null, true);
+    },
+    limits: {fileSize: 5 * 1024 * 1024}
+});
+
+export {
+    multerFileUpload,
+    multerImageUpload
+}
